8ce40fb46e3ce5ae9ee2dbc448da8e9c18d7f646
Aleksandar Garov <sasho.garov@gmail.com>
2015-06-04 22:21:22 +0300

    Initial Commit. Adding first OOP homework.

---
+++ 01. OOP-Defining-Classes-Homework/01.Persons/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Person
+{
+    private string email;
+
+    public string Name { get; set; }
+
+    public string Age { get; set; }
+
+    public string Email {
+        get
+        {
+            return this.email;
+        }
+        set
+        {
+            if (value == null || value.Contains("@"))
+            {
+                this.email = value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Invalid email!");
+            }
+        }
+    }
+
+    public Person(string name, string age) : this(name, age, null)
+    {            
+    }
+
+    public Person(string name, string age, string email)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Email = email;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Name = {0}, Age = {1}, Email = {2}", this.Name, this.Age, this.Email ?? "No email");
+    }
+}
---
+++ 01. OOP-Defining-Classes-Homework/01.Persons/PersonsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class PersonsTest
+{
+    static void Main()
+    {
+        Person g = new Person("Georgi", "Georgiev");
+        Console.WriteLine(g);
+
+        Person i = new Person("Ivan", "Ivanov", "ivan.ivanov@gmail.com");
+        Console.WriteLine(i);
+
+        Person m = new Person("Mariika", "Petrova", "gre6enemail");
+        Console.WriteLine(m);
+    }
+}
